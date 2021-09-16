063f97e941b0fd1413865e0bc9a53855f2cbb821
Zilwa <zilvinas.vidmantas.93@gmail.com>
2021-09-16 17:43:21 +0300

    files

---
+++ JS pratybų uždaviniai/classes-tasks-3.js
@@ -0,0 +1,2 @@
+// https://edabit.com/challenge/2FF7RKw9RLwc3EBY9
+// https://edabit.com/challenge/ifDM26p25bqS8EsFu
--- lesson 1 - objects/exercises/destructuring.js
+++
@@ -1,3 +0,0 @@
-const ob = { a: 7, b: 9, c: 11 };
-// Create const variables a and b in this scope, which values ar taken from <ob> object
-const { a, b } = ob;
